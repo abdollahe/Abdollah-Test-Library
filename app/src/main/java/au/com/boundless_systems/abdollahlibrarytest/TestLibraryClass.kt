11daed2e79d20240a6bc88d2dc7a30f3a2260ac3
Abdollah Ebadi <a.ebadi@boundless-systems.com.au>
2021-08-12 11:16:26 +1000

    Initial commit

---
+++ app/src/main/java/au/com/boundless_systems/abdollahlibrarytest/TestLibraryClass.kt
@@ -0,0 +1,12 @@
+package au.com.boundless_systems.abdollahlibrarytest
+
+import android.util.Log
+
+class TestLibraryClass {
+    companion object {
+        fun printTest() {
+            Log.d("Test" , "What to test")
+        }
+    }
+
+}
